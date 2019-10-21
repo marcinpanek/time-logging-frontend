af76524c706b0fd4b0dd6c5297b765e01bfb040f
Marcin <marcin@Retina-MacBook-Pro.local>
2019-10-21 23:20:43 +0200

    Updates

---
+++ precache-manifest.5dfb2a1730630a31b6edb09a740795bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79fe782ca367d5142a45ce2ba17e7da2",
+    "url": "/time-logging-frontend/index.html"
+  },
+  {
+    "revision": "627d9882a15476f8105f",
+    "url": "/time-logging-frontend/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "46b985a472b07f4e5afb",
+    "url": "/time-logging-frontend/static/js/2.5a240f08.chunk.js"
+  },
+  {
+    "revision": "627d9882a15476f8105f",
+    "url": "/time-logging-frontend/static/js/main.979ad323.chunk.js"
+  },
+  {
+    "revision": "5ba43b1ebb65bdfe6a42",
+    "url": "/time-logging-frontend/static/js/runtime-main.1b8bf262.js"
+  }
+]);
